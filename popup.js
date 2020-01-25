08ceff4518c0a31661c036a13f167533dfbf0bf9
Rounak Agarwal <rounakr.ag73@gmail.com>
2020-01-26 01:25:33 +0530

    refactor code and make link open in new tab

--- popup.js
+++ popup.js
@@ -2,23 +2,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
     fetch('https://api.codetabs.com/v1/proxy?quest=https://www.gocomics.com/random/calvinandhobbes').then((response) => response.text()).then((text) => {
         // console.log(text);
-        var el = document.createElement( 'html' );
-        el.innerHTML = text;
-        console.log(el);
-        var link=el.querySelector("link[rel='canonical']").href;
-        console.log(link);
-        var list = el.getElementsByClassName( 'item-comic-image' )[0];
-        console.log(list);
-        var fin = list.getElementsByTagName( 'img' )[0].src;
-        console.log(fin);
-        var a=document.createElement('a');
-        a.href=link;
-        a.innerHTML=link;
-        var i = document.createElement( 'img' );
-        i.src = fin;
-        var src = document.getElementById("header");
-        src.appendChild(i);
-        src.appendChild(a);
+
+        const source_html = document.createElement('html');
+        source_html.innerHTML = text;
+        // console.log(source_html);
+
+        const comic_link = source_html.querySelector("link[rel='canonical']").href;
+        // console.log(comic_link);
+
+        const img_class = source_html.getElementsByClassName('item-comic-image')[0];
+        // console.log(img_class);
+        const img_link = img_class.getElementsByTagName('img')[0].src;
+        // console.log(img_link);
+
+        const a = document.createElement('a');
+        a.href = comic_link;
+        a.target = "_blank";
+        a.innerHTML = comic_link;
+
+        const img = document.createElement('img');
+        img.src = img_link;
+
+        const comic = document.getElementById("comic");
+        comic.appendChild(a);
+        comic.appendChild(img);
     });
 
 });
